2302e9a75ebad77fc1fbf04942adda3e94bb100e
Daniel Rothfusz <dpr7@rice.edu>
2020-05-11 19:43:27 -0500

    In image: Color vs Grayscale

--- image.py
+++ image.py
@@ -2,10 +2,13 @@ import cv2
 import numpy as np
 
 ## Takes in Image, returns numpy matrix
-def import_img(path):
-    return cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+def import_img(path, grayscale):
+    if grayscale:
+        return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    else:
+        return cv2.imread(path, cv2.IMREAD_COLOR)
 
-mat = import_img('test1.png')
+mat = import_img('test1.png', True)
 print(mat)
 cv2.imshow('image', mat)
 
