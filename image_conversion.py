ac57494037bff936e0feb24a4df2612b35e7dc08
Daniel Rothfusz <dpr7@rice.edu>
2020-05-11 19:54:35 -0500

    Rename image, added option to convert colorspaces

--- image_conversion.py
+++ image_conversion.py
@@ -1,15 +1,24 @@
 import cv2
 import numpy as np
 
-## Takes in Image, returns numpy matrix
+
 def import_img(path, grayscale):
+### Takes in Image, returns numpy matrix
     if grayscale:
         return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     else:
         return cv2.imread(path, cv2.IMREAD_COLOR)
 
-mat = import_img('test1.png', True)
+# Get matrix
+mat = import_img('test1.png', False)
+
+mat_convert = cv2.cvtColor(mat, cv2.COLOR_RGB2HSV)
 print(mat)
+print(mat_convert)
+
+
+
+#Show Image
 cv2.imshow('image', mat)
 
 cv2.waitKey(0)
