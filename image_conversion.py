99e869ec608e8c55b469beedc08e4ebaa0e3866a
Daniel Rothfusz <dpr7@rice.edu>
2020-05-11 20:03:57 -0500

    Function for image display, commented print statements

--- image_conversion.py
+++ image_conversion.py
@@ -2,24 +2,32 @@ import cv2
 import numpy as np
 
 
-def import_img(path, grayscale):
+def import_img(path, is_grayscale):
 ### Takes in Image, returns numpy matrix
-    if grayscale:
+### path: string, gives path to Image
+### is_grayscale: bool, true corresponds to grayscale Output
+### RETURNS: image matrix (cv2.Mat type)
+    if is_grayscale:
         return cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     else:
         return cv2.imread(path, cv2.IMREAD_COLOR)
 
-# Get matrix
-mat = import_img('test1.png', False)
-
-mat_convert = cv2.cvtColor(mat, cv2.COLOR_RGB2HSV)
-print(mat)
-print(mat_convert)
+def display_img(img_matrix)
+### Displays the Image
+    cv2.imshow('Image', img_matrix)
 
 
+if __name__ == "__main__":
+    # Get matrix
+    matrix = import_img('test1.png', False)
 
-#Show Image
-cv2.imshow('image', mat)
+    # Testing conversion to other colorspaces
+    matrix_convert = cv2.cvtColor(mat, cv2.COLOR_RGB2HSV)
+    print(matrix)
+    print(matrix_convert)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    #Show Image
+    display_img(matrix)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
